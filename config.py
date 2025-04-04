5d8372f7bda451b0b590ae18a2892b9e0755e6b3
Hedi-hub <72658290+Hedi-hub@users.noreply.github.com>
2025-04-04 15:44:57 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GEO_KEY = os.getenv('GEO_KEY')
---
+++ weather.py
@@ -0,0 +1,21 @@
+import requests
+
+from config import GEO_KEY
+
+GEO_URL = "https://api.api-ninjas.com/v1/geocoding"  # in this website we get all lan and long of every city we want
+WEATHER_URL = "https://api.open-meteo.com/v1/forecast"
+
+class WeatherApp:
+    def __int__(self):
+        pass #affarin
+
+    def get_city_co(self):
+        pass
+
+    def get_weather_info(self):
+        pass
+
+    def suggest_clothing(self):
+        pass
+
+
